3845996434beaffd768b20f93f071aab116bcb43
okko <240953939+okkohy@users.noreply.github.com>
2025-10-31 22:19:50 +0200

    Add basic login and register pages

--- app.py
+++ app.py
@@ -4,6 +4,21 @@ import src.db
 app = Flask(__name__)
 
 
+@app.route("/login_page")
+def login_page():
+    return render_template("login_form.html")
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    return "SUCCESS!"
+
+
+@app.route("/register")
+def register():
+    return render_template("register_form.html")
+
+
 @app.route("/")
 def index():
     return render_template("index.html")
