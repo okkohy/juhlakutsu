ea636f58527ffd740e99e7200130fa26ece994b8
okko <240953939+okkohy@users.noreply.github.com>
2025-10-31 23:22:01 +0200

    Database module and register implementation

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template
-import src.db
+from flask import Flask
+from flask import redirect, render_template, request
+from werkzeug.security import generate_password_hash
+import src.db as db
+import sqlite3
 
 app = Flask(__name__)
 
@@ -17,6 +20,24 @@ def login():
 @app.route("/register")
 def register():
     return render_template("register_form.html")
+
+
+@app.route("/create", methods=["POST"])
+def create():
+    username = request.form["username"]
+    password1 = request.form["password1"]
+    password2 = request.form["password2"]
+    if password1 != password2:
+        return "VIRHE: salasanat eivät ole samat"
+    password_hash = generate_password_hash(password1)
+
+    try:
+        sql = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
+        db.execute(sql, [username, password_hash])
+    except sqlite3.IntegrityError:
+        return "VIRHE: tunnus on jo varattu"
+
+    return "Tunnus luotu"
 
 
 @app.route("/")
