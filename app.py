4bc9497107a8683d4ae2a51bc795154e8784fc86
okko <240953939+okkohy@users.noreply.github.com>
2025-11-01 10:11:52 +0200

    Basic user register and login

--- app.py
+++ app.py
@@ -1,43 +1,63 @@
 from flask import Flask
-from flask import redirect, render_template, request
+from flask import redirect, render_template, request, flash
+from flask import session
 from werkzeug.security import generate_password_hash
+import secrets
 import src.db as db
 import sqlite3
 
+import src.users as users
+
 app = Flask(__name__)
+app.secret_key = secrets.token_hex(16)
 
 
-@app.route("/login_page")
-def login_page():
-    return render_template("login_form.html")
-
-
-@app.route("/login", methods=["POST"])
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    return "SUCCESS!"
+    if request.method == "GET":
+        return render_template("login_form.html")
+
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        user_id = users.check_login(username, password)
+        if user_id:
+            session["user_id"] = user_id
+            session["username"] = username
+            session["csrf_token"] = secrets.token_hex(16)
+            # Success
+            return redirect("/")
+        else:
+            flash("VIRHE: väärä tunnus tai salasana")
+            return redirect("/login")
+    return redirect("/login")
 
 
-@app.route("/register")
+
+@app.route("/register", methods=["POST", "GET"])
 def register():
-    return render_template("register_form.html")
+    if request.method == "GET":
+        return render_template("register_form.html")
+    elif request.method == "POST":
 
+        username = request.form["username"]
+        password1 = request.form["password"]
+        password2 = request.form["password2"]
+        if password1 != password2:
+            return "VIRHE: salasanat eivät ole samat"
+        password_hash = generate_password_hash(password1)
 
-@app.route("/create", methods=["POST"])
-def create():
-    username = request.form["username"]
-    password1 = request.form["password1"]
-    password2 = request.form["password2"]
-    if password1 != password2:
-        return "VIRHE: salasanat eivät ole samat"
-    password_hash = generate_password_hash(password1)
+        try:
+            sql = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
+            db.execute(sql, [username, password_hash])
+        except sqlite3.IntegrityError:
+            flash("VIRHE: tunnus on jo varattu")
+            return redirect("/register")
 
-    try:
-        sql = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
-        db.execute(sql, [username, password_hash])
-    except sqlite3.IntegrityError:
-        return "VIRHE: tunnus on jo varattu"
-
-    return "Tunnus luotu"
+        flash("Tunnus luotu")
+        return redirect("/")
+    return redirect("/register")
 
 
 @app.route("/")
---
+++ src/users.py
@@ -0,0 +1,16 @@
+from werkzeug.security import check_password_hash
+import src.db as db
+
+
+def check_login(username: str, password: str) -> int | None:
+    sql = "SELECT id, password_hash FROM users WHERE username = ?"
+    result = db.query(sql, [username])
+    if not result:
+        return None
+
+    user_id = result[0]["id"]
+    password_hash = result[0]["password_hash"]
+    if check_password_hash(password_hash, password):
+        return user_id
+    else:
+        return None
