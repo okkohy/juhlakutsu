980686c48534ffc69f81398c08920d75b71c12ed
okko <240953939+okkohy@users.noreply.github.com>
2025-11-28 21:30:51 +0200

    add more categories to init

--- init.sql
+++ init.sql
@@ -1,7 +1,11 @@
 DELETE FROM categories;
 
 INSERT INTO categories (name) VALUES ('Syntymäpäivät');
-INSERT INTO categories (name) VALUES ('Pikkujoulut');
+INSERT INTO categories (name) VALUES ('Pikkujoulu');
+INSERT INTO categories (name) VALUES ('Joulu');
 INSERT INTO categories (name) VALUES ('Vappu');
 INSERT INTO categories (name) VALUES ('Uusi vuosi');
 INSERT INTO categories (name) VALUES ('Halloween');
+INSERT INTO categories (name) VALUES ('Juhannus');
+INSERT INTO categories (name) VALUES ('Valmistujaiset');
+INSERT INTO categories (name) VALUES ('Muu');
